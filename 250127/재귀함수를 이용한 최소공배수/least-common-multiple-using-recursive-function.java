a7504291e0e11427f1ccc644d66a1724f7bffbf8
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-01-27 22:39:36 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250127/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ 250127/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -1,36 +1,35 @@
 import java.util.*;
 public class Main {
-    public static int[] arr = new int[10];
-    public static int count = 0;
-    public static int n = 10;
-    public static int gb = 1;
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
-        n= sc.nextInt();
-        arr = new int[n];
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
 
-        System.out.print(gongBae(1));
+        int answer = arr[1];
+        for(int i=1; i<n; i++){
+            answer = llm(answer,arr[i]);
+        }
+
+        System.out.print(answer);
     }
-    
-    public static int gongBae(int i){
-        if(i >= n){
+
+    public static int llm(int a, int b){
+        return a * b / gcd(a,b);
+    }
+
+    public static int gcd(int a, int b){
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
+
+        if(max % min == 0){
+            return min;
+        }else{
             return 1;
         }
-
-        int max = Math.max(arr[i-1], arr[i]);
-        int min = Math.min(arr[i-1],arr[i]);
-
-        if(max == arr[i-1]){
-            return min * gongBae(i+1);
-        }
-
-        return max * gongBae(i+1); // 1 * 5 * 
-        
     }
 }
