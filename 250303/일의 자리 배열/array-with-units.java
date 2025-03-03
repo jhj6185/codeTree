655fe5c712ef5dd32c922e764075f51002f145d6
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-03-03 17:50:46 +0900

    Title: 일의 자리 배열, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250303/일의 자리 배열/array-with-units.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] arr= new int[10];
+        arr[0] = a;
+        arr[1] = b;
+        for(int i=2; i<10; i++){
+            arr[i] = (arr[i-2] + arr[i-1]) % 10;
+        }
+
+        for(int k : arr){
+            System.out.print(k + " ");
+        }
+    }
+}
