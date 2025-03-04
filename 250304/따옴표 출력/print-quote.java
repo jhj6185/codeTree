cd56d415bcc7f00ee2a6801415eceda15ab7119b
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-03-04 21:41:32 +0900

    Title: 따옴표 출력, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250304/따옴표 출력/print-quote.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String str = "He says \"It\'s a really simple sentence\".";
+        System.out.print(str);
+    }
+}
