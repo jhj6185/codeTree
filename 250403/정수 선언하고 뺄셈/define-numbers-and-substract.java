dc3a38cc3e94ed1d75792e853136fe9dc4df2ac5
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-03 12:35:19 +0900

    Title: 정수 선언하고 뺄셈, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250403/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int n1 = 97;
+        int n2 = 13;
+        int n3 = n1-n2;
+        System.out.print(n1 + " - " + n2 + " = " + n3);
+    }
+}
