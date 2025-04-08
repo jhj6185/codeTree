f327e6ed6032fece8c71ed838b942ef6d9e0dac3
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-08 20:08:18 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int width = sc.nextInt()+8;
+        int height= sc.nextInt()*3;
+
+        System.out.println(width);
+        System.out.println(height);
+        System.out.println(width * height);
+    }
+}
