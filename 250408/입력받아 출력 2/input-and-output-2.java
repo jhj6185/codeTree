5e8d8b1cf738dbc8c8419b8daed888556576249e
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-08 12:45:59 +0900

    Title: 입력받아 출력 2, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250408/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().replace("-","");
+
+        System.out.print(str);
+    }
+}
