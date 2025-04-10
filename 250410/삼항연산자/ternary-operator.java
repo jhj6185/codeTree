698f63412a094ed6d3c81a734be0774dc85bdae8
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-10 20:26:04 +0900

    Title: 삼항연산자, Time: 145ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250410/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num == 100){
+            System.out.print("pass");
+        }else{
+            System.out.print("failure");
+        }
+    }
+}
