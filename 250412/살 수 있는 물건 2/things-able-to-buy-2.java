b956a184b22c74b6c1774daf3b9a9325ba84fe06
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-12 00:03:37 +0900

    Title: 살 수 있는 물건 2, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n >= 3000){
+            System.out.print("book");
+        }else if(n >= 1000){
+            System.out.print("mask");
+        }else if(n >= 500){
+            System.out.print("pen");
+        }else{
+            System.out.print("no");
+        }
+    }
+}
