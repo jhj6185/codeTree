65e550d1b81a22a0a7c19687d56e10fea3f2c246
jhj6185 <96603039+jhj6185@users.noreply.github.com>
2025-04-15 20:20:25 +0900

    Title: 일 수 구하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250415/일 수 구하기/number-of-days-in-month.java
+++ 250415/일 수 구하기/number-of-days-in-month.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if(n == 1 || n == 3 || n == 5 || n == 7 || n == 10 || n == 12){
+        if(n == 1 || n == 3 || n == 5 || n == 7 || n==8 || n == 10 || n == 12){
             System.out.print(31);
         }else if(n == 2){
             System.out.print(28);
